416eafa7f2930eea09a58f9571f623cd350ccc61
Riley Castelli (she/her) <28277602+n-tropy247@users.noreply.github.com>
2022-01-17 02:18:22 -0500

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "Ryan RepRap"
+//#define MACHINE_NAME "Riley RepRap"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
