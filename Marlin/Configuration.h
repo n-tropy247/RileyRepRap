6929e2d73a08b21fd866fce43105f580c0b5fd02
Riley Castelli (she/her) <28277602+n-tropy247@users.noreply.github.com>
2022-01-17 02:17:28 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Ryan Castelli, RyanRepRap)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Riley Castelli, RyanRepRap)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -145,7 +145,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "RyanRepRap"
+#define CUSTOM_MACHINE_NAME "RileyRepRap"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
